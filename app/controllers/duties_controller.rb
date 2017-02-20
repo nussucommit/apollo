96b18ef83e2b5411cd9f410629cbdb5c3eecde0e
Gilbert Emerson <emer7>
2017-02-21 01:18:56 +0800

    Add empty action

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -1,2 +1,10 @@
 class DutiesController < ApplicationController
+  def create
+  end
+
+  def grab
+  end
+
+  def release
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def list
+  end
+  
+  def add
+  end
+
+  def view
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
