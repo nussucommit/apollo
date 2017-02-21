d027b99a3e367c440ddbc8d298fd0c94246d3004
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-02-21 22:44:29 +0800

    rubocop

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'https://github.com/commit-tech/apollo'
 
 # Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/secrets.yml',
-  'config/puma.rb'
+       'config/puma.rb'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
