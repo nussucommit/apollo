3e3e837f6cf55b7764f2a464a43757fd988c8ac6
Gilbert Emerson <emer7>
2017-02-21 08:46:56 +0800

    minor change

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -2,27 +2,28 @@ class DutiesController < ApplicationController
   def index
     date = params[:date]
     starting_date = date - (date.cwdays - 1)
-    @duties = Array.new(7)
-    starting_date.upto(starting_date + 7) do |day|
-      @duties.push(day)
-    end
+    # @duties = Array.new(7)
+    # starting_date.upto(starting_date + 7) do |day|
+    #  @duties.push(day)
+    # end
+    @duties = Array.new(starting_date..(starting_date + 7))
     @time_ranges = Time_range.all
     @places = Place.all
   end
 
   def process_grab
-    @user_id = User.find(user_id_params)
-    @duty_id = Duty.find(duty_id_params)
+    @user = User.find(user_id_params)
+    @duty = Duty.find(duty_id_params)
 
-    # user_id of duty_id should and must be null
-    redirect_to action: 'index' if @duty_id.update(@user_id)
+    # user_id of the selected duty_id should and must be null
+    redirect_to action: 'index' if @duty.update(@user)
   end
 
   def process_drop
-    @user_id = User.find(user_id_params)
-    @duty_id = Duty.find(duty_id_params)
+    @user = User.find(user_id_params)
+    @duty = Duty.find(duty_id_params)
 
-    redirect_to action: 'index' if @duty_id.update(:user_id, nil)
+    redirect_to action: 'index' if @duty.update(:user_id, nil)
   end
 
   def edit
