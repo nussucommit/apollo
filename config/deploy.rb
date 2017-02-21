ac1d0f080b5528e0046618dddf8af72b28984272
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-02-21 16:28:34 +0800

    symlink puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,8 @@ set :repo_url, 'https://github.com/commit-tech/apollo'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml',
+  'config/puma.rb'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
