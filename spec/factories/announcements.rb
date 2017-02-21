a4385ae8ca5bade13da24f0fbca5d877ddd00828
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-02-21 12:19:07 +0800

    add rubocop

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -12,8 +12,8 @@
 
 FactoryGirl.define do
   factory :announcement do
-    title "MyString"
-    contents "MyText"
-    date "2017-02-21 11:31:29"
+    title 'MyString'
+    contents 'MyText'
+    date '2017-02-21 11:31:29'
   end
 end
