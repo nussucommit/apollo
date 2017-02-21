fca18693fe92cbd3e7aa3dae4b13d50e033286f8
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-02-21 10:53:26 +0800

    make travis pass

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -1,6 +1,7 @@
 class DutiesController < ApplicationController
   def index
-    @start_time = TimeRange.order(:start).first.start
-    @end_time = TimeRange.order(:end).last.end
+    @start_time = TimeRange.order(:start).first &&
+                  TimeRange.order(:start).first.start
+    @end_time = TimeRange.order(:end).last && TimeRange.order(:end).last.end
   end
 end
--- spec/controllers/duties_controller_spec.rb
+++ spec/controllers/duties_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DutiesController, type: :controller do
+  describe 'GET #index' do
+    it 'does it successfully with' do
+      get :index
+      expect(response).to have_http_status :ok
+    end
+  end
 end
