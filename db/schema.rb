036ddb94b4ed2eb9024187b0197a5ebd9f20fa8a
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-02-21 15:23:59 +0800

    pass the specs

--- db/schema.rb
+++ db/schema.rb
@@ -61,18 +61,23 @@ ActiveRecord::Schema.define(version: 20170221040300) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "username",      null: false
-    t.string   "name",          null: false
-    t.string   "email",         null: false
-    t.string   "matric_number", null: false
-    t.string   "phone_number",  null: false
-    t.string   "cell",          null: false
-    t.string   "position",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "username",                            null: false
+    t.string   "name",                                null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "matric_number",                       null: false
+    t.string   "phone_number",                        null: false
+    t.string   "cell",                                null: false
+    t.string   "position",                            null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["matric_number"], name: "index_users_on_matric_number", unique: true, using: :btree
     t.index ["phone_number"], name: "index_users_on_phone_number", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
