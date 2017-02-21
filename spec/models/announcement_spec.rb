31fe79f2c083e5598e5aa483a7accf496c5affd6
Herbert Ilhan Tanujaya <h.i@u.nus.edu>
2017-02-21 12:15:47 +0800

    add announcement specs

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -13,5 +13,23 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'saves when nothing is null' do
+    announcement = build(:announcement)
+    expect(announcement.save).to be true
+  end
+
+  it 'does not save when title is null' do
+    announcement = build(:announcement, title: nil)
+    expect(announcement.save).to be false
+  end
+
+  it 'does not save when contents is null' do
+    announcement = build(:announcement, contents: nil)
+    expect(announcement.save).to be false
+  end
+
+  it 'does not save when date is null' do
+    announcement = build(:announcement, date: nil)
+    expect(announcement.save).to be false
+  end
 end
