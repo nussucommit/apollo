57b85d026713ce7572e6f992a4bffa168da8dee1
KhooDesmond <deskhookl@gmail.com>
2017-02-26 11:28:47 +0800

    Gemlock and spec

--- spec/controllers/announcements_controller_spec.rb
+++ spec/controllers/announcements_controller_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  contents   :text             not null
+#  date       :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe AnnouncementsController, type: :controller do
--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: timeslots
+#
+#  id              :integer          not null, primary key
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  mc_only         :boolean          not null
+#  day             :integer          not null
+#  default_user_id :integer          not null
+#  time_range_id   :integer          not null
+#  place_id        :integer          not null
+#
+# Indexes
+#
+#  index_timeslots_on_default_user_id  (default_user_id)
+#  index_timeslots_on_place_id         (place_id)
+#  index_timeslots_on_time_range_id    (time_range_id)
+#
+
 require 'rails_helper'
 
 RSpec.describe TimeslotsController, type: :controller do
