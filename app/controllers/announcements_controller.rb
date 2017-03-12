85ed56d759ef08ea59408111319718913bb80458
KhooDesmond <deskhookl@gmail.com>
2017-03-12 22:09:56 +0900

    add update and delete for announcement, introduce authorization

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,4 +1,6 @@
 class AnnouncementsController < ApplicationController
+  before_action :admin_user, only: [:create, :destroy, :update]
+
   def new
     @announcement = Announcement.new
   end
@@ -9,7 +11,7 @@ class AnnouncementsController < ApplicationController
       redirect_to announcements_path
       flash[:notice] = 'Announcement created!'
     else
-      redirect_to announcements_new_path
+      render 'new'
     end
   end
 
@@ -17,9 +19,37 @@ class AnnouncementsController < ApplicationController
     @announcements = Announcement.order(created_at: :desc)
   end
 
+  def edit
+    @announcement = Announcement.find(params[:id])
+  end
+
+  def update
+    @announcement = Announcement.find(params[:id])
+    if @announcement.update_attributes(announcement_params)
+      redirect_to announcements_path
+      flash[:notice] = 'Announcement editted!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if Announcement.find(params[:id]).destroy
+      flash[:notice] = 'You accomplished a task!'
+    else
+      flash.now[:alert] = 'There was an error deleting the task. Try again.'
+    end
+  end
+
   private
 
   def announcement_params
     params.require(:announcement).permit(:title, :contents, :date)
+  end
+
+  def admin_user
+    return unless current_user.username != 'admin'
+    redirect_to(root_url)
+    flash[:alert] = 'Access Denied'
   end
 end
