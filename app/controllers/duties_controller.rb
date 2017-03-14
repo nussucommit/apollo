9b9497caa5cbb3ef58f9474eb4478f4d7210654f
HanamaruSS <indra.rg@gmail.com>
2017-03-14 20:28:31 +0800

    duty controller herbert power yay

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -1,62 +1,38 @@
 class DutiesController < ApplicationController
-  def index
-  end
+  def index; end
 
   def process_grab
     @user = User.find(params[:user_id])
     @duty = Duty.find(params[:duty_id])
 
-    # user_id of duty_id should and must be null
-    redirect_to action: 'index' if @duty.update(@user_id)
+    # user_id of duty should and must be nil
+    if @duty.user.nil? && @duty.update(user: @user)
+      flash[:notice] = 'Successfully grabbed duty!'
+    else
+      flash[:alert] = 'Failed to grab duty!'
+    end
+    redirect_to :index
   end
 
   def process_drop
-    @user = User.find(params[:user_id])
     @duty = Duty.find(params[:duty_id])
 
-    redirect_to action: 'index' if @duty.update(:user_id, nil)
+    if @duty.update(user: nil)
+      flash[:notice] = 'Successfully dropped duty!'
+    else
+      flash[:alert] = 'Failed to drop duty!'
+    end
+    redirect_to :index
   end
 
-  def edit
-    render 'admin_edit'
+  def edit # mass edit
+    @duties = Duty.all
   end
 
   def update
-    render 'admin_update'
-  end
-
-  def admin_edit # mass edit
-    @user = User.find(params[:user_id])
-    @duties = Duty.find(params[:duty_ids])
-  end
-
-  def admin_update
-    @user = User.find(params[:user_id])
-    @duties = Duty.find(params[:duty_ids])
-    @duties.each do |duty|
-      process_grab(duty.params[:user_id], params[:duty_id])
-      unless duty.update(duty_params)
-        flash[:notice] = 'Error!'
-        render 'admin_edit'
-      end
+    @duties = params[:duties]
+    @duties.each do |duty_id, user_id|
+      Duty.find(duty_id).update(user_id: user_id)
     end
-    flash[:notice] = 'Successfully update the duty status!'
-    redirect_to root_path
-  end
-
-
-
-  private
-
-  def user_id_params
-    params.require(:users).permit(:user_id)
-  end
-
-  def duty_params
-    params.require(:duties).permit(:duty_id)
-  end
-
-  def timeslot_params
-    params.require(:timeslots).permit(:timeslot_id)
   end
 end
