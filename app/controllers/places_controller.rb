f3257927dffd61ef0183fc90464099d2ac9d7e95
HanamaruSS <indra.rg@gmail.com>
2017-03-21 19:05:52 +0800

    create new place controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,15 @@
 class PlacesController < ApplicationController
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(params[:place])
+  end
+
+  private
+    def place_params
+      params.require(:place).permit(:name)
+    end
+
 end
