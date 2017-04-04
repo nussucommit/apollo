892cb2c1697eeb45b8f6f647f24cff34307920de
Gilbert Emerson <emer7>
2017-04-04 20:01:44 +0800

    Timeslot controller finished

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,15 +1,17 @@
 class TimeslotsController < ApplicationController
-  def set_default
+
+  def mass_set_default_select
     @user = User.find(params[:user_id])
     @timeslot = Timeslot.find(params[:timeslot_id])
-
-    redirect_to action: 'index' if @timeslot.update(@user_id)
+    @duties = Duty.find(params[:duty_ids])
   end
 
-  def mass_set_default
+  def mass_set_default_process
+    @user = User.find(params[:user_id])
+    @timeslot = Timeslot.find(params[:timeslot_id])
     @duties = Duty.find(params[:duty_ids])
     @duties.each do |duty|
-      set_default(duty, duty.params[:user_id], params[:timeslot_id])
+      @duty.@timeslot.update(default_user: @user)
     end
   end
 end
--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -1,4 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe TimeslotsController, type: :controller do
+
+  describe 'GET #mass_set_default_select' do
+    it 'gets everything specified' do
+      user = create(:user)
+      timeslot = create(:timeslot)
+      duties = create(:duty)
+
+      get :mass_set_default_select, params: { user_id: user.id, timeslot_id: timeslot.id, duty_ids: duties.id }
+
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe 'PATCH #mass_set_default_process' do
+    it 'successfully mass sets default user' do
+      user = create(:user)
+      timeslot = create(:timeslot)
+      duties = create(:duty)
+
+      patch :mass_set_default_process, params: { user_id: user.id, duty_ids: duty.id, timeslot_id: timeslot.id }
+
+      expect(duty.timeslot.default_user).to eq user
+    end
+
+    it 'shows error if duty is not found' do
+      user = create(:user)
+      timeslot = create(:timeslot)
+      duties = create(:duty)
+
+      expect do
+        patch :mass_set_default_process, params: { user_id: user.id, duty_ids: duty.id, timeslot_id: timeslot.id }
+      end.to raise_error ActiveRecord::RecordNotFound
+    end
+  end
 end
